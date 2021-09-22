542f6c863c8b71f934784ff20f2d1b9a1910958c
eschroer <schroer.emerson@gmail.com>
2021-09-22 17:45:45 -0500

    Surprise me button working

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Track changes for color input 1
 document.getElementById('picker-1').oninput = function() {
-    changeColors()
+    changeColors();
 }
 //Track changes for color input 2
 document.getElementById('picker-2').oninput = function() {
@@ -20,22 +20,19 @@ function changeColors() {
   }
 
 
-
+//Generate random hexadecimal values then set them to "values" attributes of pickers
   function randomGradient() {
-    let [inputColorX, inputColorY, inputColorZ] = [Math.floor(Math.random() * 226), Math.floor(Math.random() * 226), Math.floor(Math.random() * 226)]
-    let inputColor1 = document.getElementById('picker-1').innerHTML
-    inputColor1 = `rgb(${inputColorX}, ${inputColorY}, ${inputColorZ})`
+    let inputColorX = Math.floor(Math.random() * 16777215).toString(16)
+    let inputColor1 = document.getElementById('picker-1')
+    inputColor1.setAttribute('value', `#${inputColorX}`)
     
-    let [inputColorA, inputColorB, inputColorC] = [Math.floor(Math.random() * 226), Math.floor(Math.random() * 226), Math.floor(Math.random() * 226)]
-    let inputColor2 = document.getElementById('picker-2').value
-    inputColor2 = `rgb(${inputColorA}, ${inputColorB}, ${inputColorC})`
+    let inputColorY = Math.floor(Math.random() * 16777215).toString(16)
+    let inputColor2 = document.getElementById('picker-2')
+    inputColor2.setAttribute('value', `#${inputColorY}`)
     
-    //Right now works if you adjust before clicking "Surprise Me" but not after because inputs are not actually changed
     let sliderVal = document.getElementById('split').value
 
-
-    console.log(inputColor1, inputColor2)
-    document.getElementById('gradient').style.background = `linear-gradient(${inputColor1} ${sliderVal}%, ${inputColor2})`
+    document.getElementById('gradient').style.background = `linear-gradient(#${inputColorX} ${sliderVal}%, #${inputColorY})`
   }
 
 
